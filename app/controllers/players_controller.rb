60825e6e24854ea9caac2185a1d11d4cca61033f
elmi- <abdi.elmi@rocketmail.com>
2022-01-19 12:58:35 -0800

    add most recent notes to Player#index

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,10 @@
 class PlayersController < ApplicationController
   def index
     @players = Player.all
-    @latest_notes = Note.limit(5).order(note_date: :desc)
+
+    @notes = Note.all.order(note_date: :desc)
     # byebug
+    @latest_notes = Note.limit(5).order(note_date: :desc)
   end
 
   def show
