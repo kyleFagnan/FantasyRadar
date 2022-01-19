af4f837d5f642775242dcb7520273a00694ca1ce
bhalemegha <your@email.com>
2022-01-19 22:58:07 +0000

    fix redirect issue in signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if user.save
+    if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to root_path
     else
       redirect_to '/signup'
   end
