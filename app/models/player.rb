37c61b57888af96810e901e75caf8fe08a0f8a0c
bhalemegha <your@email.com>
2022-01-21 21:24:05 +0000

    Add search feature

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,13 @@
 class Player < ActiveRecord::Base
   has_many :Subscription
   has_many :notes, dependent: :delete_all
+
+  def self.search(search)
+    if search
+          @searchedPlayer = Player.where("player_name LIKE ?", "%#{search}%")
+    else
+      @searchedPlayer = Player.all
+    end
+    @searchedPlayer
+  end
 end
