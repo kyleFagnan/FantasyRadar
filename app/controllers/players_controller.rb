6d5d641e5c95827745215d1dcc9dc00cd51b13d8
elmi- <abdi.elmi@rocketmail.com>
2022-01-22 12:48:34 -0800

    added logic to check user Subscriptions and send notifications if new notes detected

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,8 +3,8 @@ class PlayersController < ApplicationController
     @players = Player.all
 
     @notes = Note.all.order(note_date: :desc)
-    # byebug
     @latest_notes = Note.limit(5).order(note_date: :desc)
+    # PlayersHelper::getNewNotes()
   end
 
 
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,5 @@
 class SubscriptionsController < ApplicationController
   def index
-
     # @favourite_notes = Subscription.favourite_notes
     @my_subscriptions = Subscription.where(user_id: session[:user_id])
     @my_players = []
